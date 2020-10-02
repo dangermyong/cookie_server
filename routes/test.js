9c0fa07f67f00540322c1d9af1518afa7b19ed88
dangermyong <66460964+dangermyong@users.noreply.github.com>
2020-10-02 23:12:54 +0900

    a

--- routes/test.js
+++ routes/test.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   const maxAge = 24 * 60 * 60 * 10;
   const token = 'cookie practice'
 
-  res.cookie('jwt', token, { httpOnly: true, sameSite: 'none', secure: true, maxAge: maxAge * 1000 })
+  res.cookie('jwt', token, { httpOnly: true, sameSite: 'none', maxAge: maxAge * 1000 })
   res.status(200).send('respond with a resource');
 });
 
