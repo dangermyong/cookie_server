69cfb1ba48c34c4c52ed9ccba64a2b261c2d631a
dangermyong <66460964+dangermyong@users.noreply.github.com>
2020-10-02 22:30:52 +0900

    a

--- app.js
+++ app.js
@@ -21,10 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true,
-}))
+app.use(cors())
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
