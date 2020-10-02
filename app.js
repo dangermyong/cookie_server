01a2f800b48eb9b4311d7a110b103e20764d669b
dangermyong <66460964+dangermyong@users.noreply.github.com>
2020-10-02 22:23:15 +0900

    asdf

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors())
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Credentials", true)
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Credentials", true)
 
   next()
 })
