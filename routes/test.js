1846393dc8ccf61ca91f0c0fbd84f18293b858f9
dangermyong <66460964+dangermyong@users.noreply.github.com>
2020-10-02 14:15:26 +0900

    first commit

---
+++ routes/test.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  const maxAge = 24 * 60 * 60 * 10;
+  const token = 'cookie practice'
+
+  res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 })
+  res.status(200).send('respond with a resource');
+});
+
+module.exports = router;
